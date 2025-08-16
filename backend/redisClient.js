8ae3f8c944a1330acabece8bb37880c743eb7769
SaiKumarReddy374 <saikumarreddypalakolanu6@gmail.com>
2025-08-16 19:33:25 +0530

    first Commit

---
+++ backend/redisClient.js
@@ -0,0 +1,13 @@
+// redisClient.js
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+
+const client = createClient({
+  url: process.env.REDIS_URL,
+});
+
+client.on('error', (err) => console.error('Redis Client Error', err));
+
+await client.connect();
+
+export default client;
